a6656a4ec2fb51e74389e737e4e5af3439c883f6
SameerJaipal <sj06422@st.habib.edu.pk>
2024-02-18 23:10:55 +0500

    code

---
+++ src/components/pages/Login.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../../App.css';
+
+export default function login() {
+  return <h1 className='login'>This is the login page</h1>;
+}
---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../../App.css';
+
+export default function SignUp() {
+  return <h1 className='sign-up'>This is the SignUp page</h1>;
+}
