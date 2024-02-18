355ed16184e9f7b9a4e0782d0a68777ec32b86f2
SameerJaipal <77678240+SameerJaipal@users.noreply.github.com>
2024-02-19 00:45:48 +0500

    Box and image added

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,6 +1,116 @@
-import React from 'react';
-import '../../App.css';
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import toast from "react-hot-toast";
+import axios from "axios";
+import {
+  Box,
+  Typography,
+  useTheme,
+  useMediaQuery,
+  TextField,
+  Button,
+  Alert,
+  Collapse,
+} from "@mui/material";
 
-export default function login() {
-  return <h1 className='login'>This is the login page</h1>;
-}
+const Login = () => {
+  const theme = useTheme();
+  const history = useHistory();
+  //media
+  const isNotMobile = useMediaQuery("(min-width: 1000px)");
+  // states
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  //register ctrl
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/api/v1/auth/login", { email, password });
+      toast.success("Login Successfully");
+      localStorage.setItem("authToken", true);
+      history.push("/"); // Redirect to home page
+    } catch (err) {
+      console.log(error);
+      if (err.response.data.error) {
+        setError(err.response.data.error);
+      } else if (err.message) {
+        setError(err.message);
+      }
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+    }
+  };
+
+  return (
+    <div
+      style={{
+        backgroundImage: `url('/images/img-home.jpg')`, // Change this to your image path
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Box
+        width={isNotMobile ? "40%" : "80%"}
+        p={"2rem"}
+        borderRadius={5}
+        sx={{
+          boxShadow: 5,
+          backgroundColor: "white", // Change background color to white
+        }}
+      >
+        <Collapse in={error}>
+          <Alert severity="error" sx={{ mb: 2 }}>
+            {error}
+          </Alert>
+        </Collapse>
+        <form onSubmit={handleSubmit}>
+          <Typography variant="h3">Login</Typography>
+
+          <TextField
+            label="email"
+            type="email"
+            required
+            margin="normal"
+            fullWidth
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <TextField
+            label="password"
+            type="password"
+            required
+            margin="normal"
+            fullWidth
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            size="large"
+            sx={{ color: "white", mt: 2 }}
+          >
+            Login
+          </Button>
+          <Typography mt={2}>
+            Don't have an account? <Link to="/sign-up">Please Register</Link>
+          </Typography>
+        </form>
+      </Box>
+    </div>
+  );
+};
+
+export default Login;
